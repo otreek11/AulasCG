ebd9f4afde3e660207a7ce11f0e6c7e90c608008
oTreeK <otreek11@gmail.com>
2025-09-30 23:36:07 -0300

    exercicio triangle_02

---
+++ triangle_02/README.md
@@ -0,0 +1,3 @@
+### Triângulos (Exercício 01)
+
+&emsp;Exercício sequencial do triangle_01, onde o intuito é além de posicionar os dois triângulos também os colorir com interpolação de cores nos vértices
---
+++ triangle_02/triangle.js
@@ -0,0 +1,81 @@
+
+var gl;
+var points;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    
+    var vertices = new Float32Array([
+        -1, -1, 
+        -0.5, 0, 
+        0, -1,
+
+        0, -1,
+        0.5, 0,
+        1, -1,
+
+        -0.5, 0,
+        0, 1,
+        0.5, 0,
+    ]);
+
+    var colors = new Float32Array([
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,
+
+        0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0,
+        1.0, 0.0, 0.0,
+
+        0.0, 1.0, 0.0,
+        1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0,
+    ]);
+
+    //  Configure WebGL
+    
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER,vertices, gl.STATIC_DRAW );
+
+    
+    // Associate out shader variables with our data buffer
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+    
+    var colorBufID = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBufID);
+    gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+    
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+    
+    
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, 9 );
+}
